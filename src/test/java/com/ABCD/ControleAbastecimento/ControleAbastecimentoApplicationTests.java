ee1e675a265944f8b8a10138c745b5319ef2a04d
Adauto Moisés <adautomaleandro@gmail.com>
2024-02-21 21:03:55 -0300

    iniciando repositório

---
+++ src/test/java/com/ABCD/ControleAbastecimento/ControleAbastecimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABCD.ControleAbastecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAbastecimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
